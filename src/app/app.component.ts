0608aefe5d91d9e0e78868dc5a00060dd94a0227
sriram bala <sr.bala.kkl@gmail.com>
2022-01-07 13:29:31 +0530

    ADD:nested FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Max-ReactiveForms';
+export class AppComponent implements OnInit {
+    title = 'Max-ReactiveForms';
+    genders = ['male', 'female'];
+
+    signUpForm!: FormGroup;
+
+    //todo:un commended method is the angular.io recommended method
+
+    // ngOnInit() {
+    //     this.signUpForm = new FormGroup({
+    //         'username': new FormControl(null, [Validators.required]),
+    //         'email': new FormControl(null),
+    //         'gender': new FormControl('male')
+    //     });
+    //     console.log(this.signUpForm)
+    // }
+
+    ngOnInit() {
+        this.signUpForm = new FormGroup({
+            username: new FormControl(null, [Validators.required]),
+            email: new FormControl(null),
+            gender: new FormControl('male'),
+
+            address: new FormGroup({
+                streetFCN:new FormControl(null,[Validators.required]),
+                pincodeFCN: new FormControl(null,Validators.required),
+                localityFCN: new FormControl(null,Validators.required),
+                addressFCN : new FormControl(null,Validators.required),
+            }),
+        });
+        console.log(this.signUpForm)
+    }
+
+    onsubmit() {
+        console.log(this.signUpForm.value)
+        console.log(this.signUpForm)
+    }
 }
